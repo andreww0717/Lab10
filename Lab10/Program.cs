316290433912c2da983fd265b16e4e33729e80b5
andreww980 <andreww0717@outlook.com>
2021-03-28 14:59:44 -0400

    Add project files.

---
+++ Lab10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string userInput;
+        Console.Write("Enter a string:");
+        userInput = Console.ReadLine();
+        vowelRemover(userInput);
+    }
+
+    public static void vowelRemover(string userInput)
+    {
+        int count = 0;
+        for (int i = 0; i < userInput.Length; i++)
+        {
+            switch (userInput[i])
+            {
+                case 'a':
+                    count++;
+                    Console.Write("*"); break;
+                case 'e':
+                    count++;
+                    Console.Write("*"); break;
+                case 'i':
+                    count++;
+                    Console.Write("*"); break;
+                case 'o':
+                    count++;
+                    Console.Write("*"); break;
+                case 'u':
+                    count++;
+                    Console.Write("*"); break;
+                default:
+                    Console.Write(userInput[i]);
+                    break;
+            }
+        }
+        Console.WriteLine($"\nThat string has {count} vowels.");
+    }
+}
+
---
+++ Lab10B/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab10B
+{
+    class Program
+    {
+        public static void printCensoredString(string userInput)
+        {
+            string badWord = "dang";
+        string NewWord = "&^#@";
+
+            while (userInput.IndexOf(badWord) != -1)
+            {
+                int index = userInput.IndexOf(badWord);
+                Console.WriteLine($"Found bad word at: {index}");
+                userInput = userInput.Remove(index, 4).Insert(index, NewWord.ToString());
+            }
+    Console.WriteLine(userInput);
+            
+        }
+        static void Main(string[] args)
+{
+    Console.Write("Enter a string: ");
+    String userInput = Console.ReadLine();
+            printCensoredString(userInput);
+
+}
+
+
+    }
+}
